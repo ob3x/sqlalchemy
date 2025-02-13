cadfbfca7a3564a234810279c5bee12b27d5be1c
ob3x <ob3x.biznes@gmail.com>
2025-02-13 23:18:59 +0100

    test

--- library/main.py
+++ library/main.py
@@ -10,16 +10,21 @@ app = FastAPI()
 
 @app.post("/add-books/", response_model=Book)
 def create_book(book : CreateBook, db : Session = Depends(get_db)):
-    new_book = Books(title = book.title, 
-                 author = book.author, 
-                 description = book.description, 
-                 year = book.year)
+    new_book = Books(
+                title = book.title, 
+                author = book.author, 
+                description = book.description, 
+                year = book.year)
     
     db.add(new_book)
     db.commit()
     db.refresh(new_book)
 
     return {"Book" : new_book}
+
+# @app.get("/books/")
+# def get_books(db : Session = Depends(get_db)):
+#     return db.query()
 
 if __name__ == "__main__":
     uvicorn.run(app)
